eed5a8aacfdfd71eb1254c32815d5b8ba894318c
Vladimir Kovpak <vladimirk@ufins.com>
2014-03-11 19:27:54 +0200

    Skipe. Added insert into mongo db at registration.

--- nodeJs/skipe/app.js
+++ nodeJs/skipe/app.js
@@ -2,14 +2,18 @@
 
 var express = require('express');
 var expressValidator = require('express-validator');
+var mongodb = require('mongodb');
+var mongoServer = new mongodb.Server('localhost', 27017, {auto_reconnect: true});
+var mongoDB = new mongodb.Db('skipe', mongoServer);
 var app = express();
 
-app.configure(function(){
+app.configure(function () {
     app.use(express.static(__dirname + '/public'));
     app.use(express.bodyParser());
     app.use(expressValidator());
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
+    app.set('mongoDB', mongoDB);
 });
 
 app.get('*', require('./routes/app').go);
--- nodeJs/skipe/routes/guest.js
+++ nodeJs/skipe/routes/guest.js
@@ -1,11 +1,34 @@
 exports.registration = function (req, res) {
     req.checkBody('email', 'Invalid email').isEmail();
-    req.checkBody('sname', 'Invalid sname').is(/^[\w\s\_\-\=\+@]+$/);
+    req.checkBody('sname', 'Invalid sname').matches(/^[\w\s\_\-\=\+@]+$/); // or .is()
     var e = req.validationErrors();
     if (e) {
         res.json({errors: e});
         return;
     }
-    // console.log(req.param('email'));
-    // MONGO
+    var mongodb = require('mongodb');
+    var mongoServer = new mongodb.Server('localhost', 27017, {auto_reconnect: true});
+    var mongoDB = new mongodb.Db('skipe', mongoServer);
+    mongoDB.open(function(err, db) {
+        if (err) {
+            console.log(err);
+        } else {
+            db.collection('user', function(err, collection) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    collection.insert({
+                        email: req.param('email'),
+                        sname: req.param('sname'),
+                    }, function(err, docs) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            res.json({success: docs});
+                        }
+                    });
+                }
+            });
+        }
+    });
 };
