fa8c37e1d5a0ff3ae9b9508f22c8135d617f33dd
Vladimir Kovpak <vladimirk@ufins.com>
2014-04-23 19:54:55 +0300

    Up _ php helper.

--- _/php/pp-prepare_projects.php
+++ _/php/pp-prepare_projects.php
@@ -1,10 +1,12 @@
 <?
 
-if (isset($argv[1]))
+if (isset($argv[1])) {
     define('PROJECT', $argv[1]);
-else
+} else {
     die('Set project argument and try again. [m,a]');
+}
 
+/*
 // PhpStorm
 if (PROJECT == 's') {
     echo "PhpStorm\n";
@@ -17,11 +19,13 @@ if (PROJECT == 's') {
     }
     exit();
 }
+*/
 
 // AFFILIATES
 if (PROJECT == 'a') {
     echo "AFFILIATES\n";
     define('DIR', '/home/kovpak/web/kovpak/aff/');
+
     //.htaccess
     $file = DIR . '.htaccess';
     $text = file_get_contents($file);
@@ -32,6 +36,7 @@ if (PROJECT == 'a') {
     }
     $str1 = file_put_contents($file, $text) ? '+' : '-';
     echo 'success: ' . $str1 . ' | ' . $file . "\n";
+
     //.htaccess help_scripts
     $file = DIR . 'help_scripts/.htaccess';
     $text = file_get_contents($file);
@@ -42,6 +47,7 @@ if (PROJECT == 'a') {
     }
     $str1 = file_put_contents($file, $text) ? '+' : '-';
     echo 'success: ' . $str1 . ' | ' . $file . "\n";
+
     // debug functions
     $file = DIR . 'include/debug/functions.php';
     $text = file_get_contents($file);
@@ -104,11 +110,32 @@ if (PROJECT == 'a') {
 if (PROJECT == 'm') {
     echo "MULTISITE\n";
     define('DIR', '/home/kovpak/web/kovpak/multi_web/');
+
     // .htaccess
     $file = DIR . '.htaccess';
     $text = file_get_contents($file);
     $str1 = 'php_value display_errors on';
     $str2 = 'php_value display_errors off';
+    if (strpos($text, $str1) !== false) $text = str_replace($str1, $str2, $text);
+    else $text = str_replace($str2, $str1, $text);
+    $str1 = file_put_contents($file, $text) ? '+' : '-';
+    echo 'success: ' . $str1 . ' | ' . $file . "\n";
+
+    // cron/.htaccess
+    $file = DIR . 'cron/.htaccess';
+    $text = file_get_contents($file);
+    $str1 = "Order deny,allow\nAllow from 127.0.0.1";
+    $str2 = "Order deny,allow\nDeny from All\nAllow from 127.0.0.1";
+    if (strpos($text, $str1) !== false) $text = str_replace($str1, $str2, $text);
+    else $text = str_replace($str2, $str1, $text);
+    $str1 = file_put_contents($file, $text) ? '+' : '-';
+    echo 'success: ' . $str1 . ' | ' . $file . "\n";
+
+    // inc/mysql.inc.php
+    $file = DIR . 'inc/mysql.inc.php';
+    $text = file_get_contents($file);
+    $str1 = "!defined('dbg') or defined('nodbg') or dbg != 1 or print('<span style=\"color:steelblue\"><pre>'.str_ireplace(' from ', \"<b style='color:crimson'> from </b>\", \$sql->show()).';<hr></pre></span>');\n        !defined('dbg') or defined('nodbg') or dbg != 2 or print(\$sql->show().\"\\n-----------------------------------------------------------------------------------------------------------------\\n\");\n        \$balancing_mode = empty(\$data['connection_params']['balancing_mode'])";
+    $str2 = "\$balancing_mode = empty(\$data['connection_params']['balancing_mode'])";
     if (strpos($text, $str1) !== false) $text = str_replace($str1, $str2, $text);
     else $text = str_replace($str2, $str1, $text);
     $str1 = file_put_contents($file, $text) ? '+' : '-';
@@ -124,6 +151,17 @@ if (PROJECT == 'm') {
     $str1 = file_put_contents($file, $text) ? '+' : '-';
     echo 'success: ' . $str1 . ' | ' . $file . "\n";
     */
+
+    // inc/debug.inc.php
+    $file = DIR . 'inc/debug.inc.php';
+    $text = file_get_contents($file);
+    $str1 = 'pr($errType.$errArrMAIL["Error message"],$errArrMAIL,$errTimeReal,$errTimeLog,$sending);';
+    $str2 = 'send_debug_mail($errType,$errArrMAIL,$errTimeReal,$errTimeLog,$sending);';
+    if (strpos($text, $str1) !== false) $text = str_replace($str1, $str2, $text);
+    else $text = str_replace($str2, $str1, $text);
+    $str1 = file_put_contents($file, $text) ? '+' : '-';
+    echo 'success: ' . $str1 . ' | ' . $file . "\n";
+
     // inc/debug.inc.php
     $file = DIR . 'inc/debug.inc.php';
     $text = file_get_contents($file);
@@ -141,9 +179,19 @@ if (PROJECT == 'px') {
     define('DIR', '/home/kovpak/web/kovpak/px/');
 
     // .htaccess
+    // $file = DIR . '.htaccess';
+    // $text = file_get_contents($file);
+    // $str1 = "allow from all\n";
+    // $str2 = "";
+    // if (strpos($text, $str1) !== false) $text = str_replace($str1, $str2, $text);
+    // else $text = $str1 . $text;
+    // $str1 = file_put_contents($file, $text) ? '+' : '-';
+    // echo 'success:
+
+    // .htaccess xDebug
     $file = DIR . '.htaccess';
     $text = file_get_contents($file);
-    $str1 = "allow from all\n";
+    $str1 = "php_value xdebug.remote_enable 1\n";
     $str2 = "";
     if (strpos($text, $str1) !== false) $text = str_replace($str1, $str2, $text);
     else $text = $str1 . $text;
@@ -161,23 +209,23 @@ if (PROJECT == 'px') {
     echo 'success: ' . $str1 . ' | ' . $file . "\n";
 
     // dataCollector
-    $file = DIR . 'protected/config/dataCollector.php';
-    $text = file_get_contents($file);
-    $str1 = "'eAffiliates'                => ['priority' => 'urgent',";
-    $str2 = "'eAffiliates'                => [";
-    if (strpos($text, $str1) !== false) $text = str_replace($str1, $str2, $text);
-    else $text = str_replace($str2, $str1, $text);
-    $str1 = file_put_contents($file, $text) ? '+' : '-';
+    // $file = DIR . 'protected/config/dataCollector.php';
+    // $text = file_get_contents($file);
+    // $str1 = "'eAffiliates'                => ['priority' => 'urgent',";
+    // $str2 = "'eAffiliates'                => [";
+    // if (strpos($text, $str1) !== false) $text = str_replace($str1, $str2, $text);
+    // else $text = str_replace($str2, $str1, $text);
+    // $str1 = file_put_contents($file, $text) ? '+' : '-';
 
     // debugToolbar
-    $file = DIR . 'protected/extensions/yii-debug-toolbar/assets/yii.debug.toolbar.js';
-    $text = file_get_contents($file);
-    $str1 = "$('#yii-debug-toolbar').hide(); this.initTabs();";
-    $str2 = "this.initTabs();";
-    if (strpos($text, $str1) !== false) $text = str_replace($str1, $str2, $text);
-    else $text = str_replace($str2, $str1, $text);
-    $str1 = file_put_contents($file, $text) ? '+' : '-';
-    echo 'success: ' . $str1 . ' | ' . $file . "\n";
+    // $file = DIR . 'protected/extensions/yii-debug-toolbar/assets/yii.debug.toolbar.js';
+    // $text = file_get_contents($file);
+    // $str1 = "$('#yii-debug-toolbar').hide(); this.initTabs();";
+    // $str2 = "this.initTabs();";
+    // if (strpos($text, $str1) !== false) $text = str_replace($str1, $str2, $text);
+    // else $text = str_replace($str2, $str1, $text);
+    // $str1 = file_put_contents($file, $text) ? '+' : '-';
+    // echo 'success: ' . $str1 . ' | ' . $file . "\n";
 
     // SQL Log 1
     $file = DIR . 'protected/config/main.php';
@@ -199,7 +247,7 @@ if (PROJECT == 'px') {
     $str1 = file_put_contents($file, $text) ? '+' : '-';
     echo 'success: ' . $str1 . ' | ' . $file . "\n";
 }
-
+/*
 $file = '/usr/bin/geany_function_1';
 $text = '#!/bin/bash'."\n".'s=`xargs -0 echo`'."\n".'geany '.DIR.'$s';
 $str1 = file_put_contents($file, $text) ? '+' : '-';
@@ -212,5 +260,5 @@ $file = '/usr/bin/geany_function_3';
 $text = '#!/bin/bash'."\n".'s=`xargs -0 echo`'."\n".'find '.DIR.' -iname  "*$s*"';
 $str1 = file_put_contents($file, $text) ? '+' : '-';
 echo 'success: ' . $str1 . ' | ' . $file . "\n";
-
+*/
 ?>
