8986e65bb0281918f95b37e44ac5ffa370cd0898
Vladimir <jb007aston@gmail.com>
2015-01-11 15:07:34 +0200

    Skipe. Added mongo fixture.

---
+++ skipe/mongo.js
@@ -0,0 +1,11 @@
+// Users
+db.user.insert({ "_id" : ObjectId("53862c14d8b285f677d0d45a"), "email" : "codenamek2010@gmail.com", "sname" : "cn", "password" : "82d7dcf5a4" });
+db.user.insert({ "_id" : ObjectId("54b23de857fe2afb0c1182bf"), "email" : "codenamek2010+JamesBond@gmail.com", "sname" : "James Bond", "password" : "fee8edd194" });
+db.user.insert({ "_id" : ObjectId("54b23e6ab8b3cf0211c5adf3"), "email" : "codenamek2010+M@gmail.com", "sname" : "M", "password" : "de3a50b65c" });
+db.user.insert({ "_id" : ObjectId("54b24d47df23943812d375fc"), "email" : "codenamek2010+Q@gmail.com", "sname" : "Q", "password" : "91d11f0dc4" });
+db.user.insert({ "_id" : ObjectId("54b24dae445e086e129d0feb"), "email" : "codenamek2010+VesperLynd@gmail.com", "sname" : "Vesper Lynd", "password" : "ae95511c7c" });
+db.user.insert({ "_id" : ObjectId("54b24e095b9581dc127f428f"), "email" : "codenamek2010+FelixLeiter@gmail.com", "sname" : "Felix Leiter", "password" : "6c4e16ede8" });
+db.user.insert({ "_id" : ObjectId("54b24e99af762f8013a30525"), "email" : "codenamek2010+ReneMathis@gmail.com", "sname" : "Rene Mathis", "password" : "bc9b7f38a9" });
+db.user.insert({ "_id" : ObjectId("54b24f95061695e114e9910f"), "email" : "codenamek2010+AlexDimitrios@gmail.com", "sname" : "Alex Dimitrios", "password" : "db3815b6ca" });
+db.user.insert({ "_id" : ObjectId("54b24fbe49b8802715620d76"), "email" : "codenamek2010+LeChiffre@gmail.com", "sname" : "Le Chiffre", "password" : "6464688c35" });
+db.user.insert({ "_id" : ObjectId("54b24fe82607dbaf153f9afc"), "email" : "codenamek2010+MrWhite@gmail.com", "sname" : "Mr White", "password" : "83d5645f59" });
