6916903e82e2c158ed48176c760ce7993e32df6e
Vladimir Kovpak <cn007b@gmail.com>
2015-01-13 12:25:39 +0200

    Update oneLineCode.js

--- ed/js/examples/oneLineCode.js
+++ ed/js/examples/oneLineCode.js
@@ -2,3 +2,6 @@
 6 - '1'     // 5     - as int.
 "3" + 4 + 5 // "345" - as string.
 3 + 4 + "5" // "75"  - as int and as string.
+
+if (true) {} elseif (true) {} // Uncaught SyntaxError: Unexpected token {
+if (true) {} else if (true) {} // OK.
