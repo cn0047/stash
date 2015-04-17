75b8441d99f26f707f06240229f730c3f11f5a64
Vladimir <cn007b@gmail.com>
2015-04-17 12:21:05 +0300

    Ed. Php.

---
+++ ed/php/examples/pdo.insert.php
@@ -0,0 +1,53 @@
+<?php
+
+$dbh = new PDO('mysql:dbname=test;host=127.0.0.1', 'root');
+
+/**
+ * VARIANT 1
+ *
+ * Everything OK.
+ */
+$sth = $dbh->prepare('INSERT INTO stockists VALUES (null, :s1, :s2)');
+$sth->bindValue(':s1', uniqid(), PDO::PARAM_STR);
+$sth->bindValue(':s2', uniqid(), PDO::PARAM_STR);
+if (!$sth->execute()) {
+    var_dump($sth->errorInfo());
+} else {
+    echo 'VARIANT 1 - OK, at: '.time();
+}
+
+/**
+ * VARIANT 2
+ *
+ * Everything OK.
+ */
+$sth = $dbh->prepare('INSERT INTO stockists VALUES (null, :s1, :s2)');
+if (!$sth->execute([':s1' => uniqid(), ':s2' => uniqid()])) {
+    var_dump($sth->errorInfo());
+} else {
+    echo 'VARIANT 2 - OK, at: '.time();
+}
+
+/**
+ * VARIANT 3
+ *
+ * Everything OK.
+ */
+$sth = $dbh->prepare('INSERT INTO stockists (state1, state2) VALUES (:s1, :s2)');
+if (!$sth->execute([':s1' => uniqid(), ':s2' => uniqid()])) {
+    var_dump($sth->errorInfo());
+} else {
+    echo 'VARIANT 3 - OK, at: '.time();
+}
+
+/**
+ * VARIANT 4
+ *
+ * Everything OK.
+ */
+$sth = $dbh->prepare('INSERT INTO stockists SET state1 = :s1, state2 = :s2');
+if (!$sth->execute([':s1' => uniqid(), ':s2' => uniqid()])) {
+    var_dump($sth->errorInfo());
+} else {
+    echo 'VARIANT 4 - OK, at: '.time();
+}
