6bb83e34d8e3587caa162a55d99ca61f9cbf9886
Vladimir <jb007aston@gmail.com>
2015-05-16 23:19:49 +0300

    Ed. Yii - sub module.

---
+++ ed/yii/examples/testdrive/protected/modules/admin/modules/subadmin/SubadminModule.php
@@ -0,0 +1,5 @@
+<?php
+
+class SubadminModule extends CWebModule
+{
+}
---
+++ ed/yii/examples/testdrive/protected/modules/admin/modules/subadmin/controllers/admin/IndexController.php
@@ -0,0 +1,9 @@
+<?php
+
+class IndexController extends CController
+{
+    public function actionIndex()
+    {
+        var_dump('subadmin');
+    }
+}
