3bf073f61a28682bae02c3f654ba2b942e1a94ec
Vladimir Kovpak <cn007b@gmail.com>
2015-09-28 18:48:03 +0300

    Ed. Mysql

--- ed/mySql/varCharAsUniqueKey.sql
+++ ed/mySql/varCharAsUniqueKey.sql
@@ -6,7 +6,7 @@ create table uk(
 );
 insert into uk values (null, 'firefox');
 insert into uk values (null, 'Chrome');
-insert into uk values (null, 'Firefox')
+insert into uk values (null, 'Firefox');
 -- ERROR 1062 (23000): Duplicate entry 'Firefox' for key 'browser'
 select * from uk;
 +----+---------+
@@ -16,3 +16,23 @@ select * from uk;
 |  1 | firefox |
 +----+---------+
 2 rows in set (0.00 sec)
+
+
+
+create table uk2 (
+    id int Auto_increment,
+    browser varchar(20) CHARACTER SET utf8 COLLATE utf8_bin,
+    primary key (id),
+    unique key (browser)
+);
+insert into uk2 values (null, 'firefox');
+insert into uk2 values (null, 'Chrome');
+insert into uk2 values (null, 'Firefox');
+select * from uk2;
++----+---------+
+| id | browser |
++----+---------+
+|  2 | Chrome  |
+|  3 | Firefox |
+|  1 | firefox |
++----+---------+
