100124287ec30f6b3710f5806d0d6b4d2acaee5e
Vladimir Kovpak <cn007b@gmail.com>
2015-10-22 00:25:37 +0300

    Ed. Php, added new pdo example.

---
+++ ed/php/examples/pdo.fetchClass.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+select * from user;
++----+----------------+------+--------------------------------------------------------------+
+| id | email          | name | password                                                     |
++----+----------------+------+--------------------------------------------------------------+
+|  1 | test@gmail.com | Test | $2y$10$/UlSlT.bCntE1L2099KnmeM4I1v1Vjr/278UDxIghCeBndVnfjD.y |
+|  2 | bond@mi6.com   | Bond | $2y$10$DK4NjvRPICtM57e..V7hlOr6sVKAvwbm1M3u.uFrJsLIWkO6R179m |
++----+----------------+------+--------------------------------------------------------------+
+*/
+
+class User
+{
+    public function __toString()
+    {
+        return sprintf('Name: %s, Email: %s'.PHP_EOL, $this->name, $this->email);
+    }
+}
+
+$dbh = new PDO('mysql:dbname=test;host=127.0.0.1', 'root');
+$sth = $dbh->prepare('SELECT * FROM user');
+$sth->execute();
+$result = $sth->fetchAll(PDO::FETCH_CLASS, 'User');
+foreach ($result as $obj) {
+    echo $obj;
+}
+
+/*
+Name: Test, Email: test@gmail.com
+Name: Bond, Email: bond@mi6.com
+*/
