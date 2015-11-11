bf3128d5343677c3a5f7420e8472d69f72da2540
Vladimir Kovpak <cn007b@gmail.com>
2015-11-12 01:37:10 +0200

    Ed. Added php google auth example.

---
+++ ed/php/examples/googleAuth/index.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+$clientId = '';
+$clientSecret = '';
+$redirectUrl = '';
+$homeUrl = '';
+$applicationName = '';
+
+session_start();
+
+$googleClient = new Google_Client();
+$googleClient->setApplicationName($applicationName);
+$googleClient->setClientId($clientId);
+$googleClient->setClientSecret($clientSecret);
+$googleClient->setRedirectUri($redirectUrl);
+$googleClient->setScopes('https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email');
+
+if (isset($_GET['code'])) {
+    $googleClient->authenticate($_GET['code']);
+    $_SESSION['token'] = $googleClient->getAccessToken();
+    header("Location: $redirect_url");
+}
+if (isset($_SESSION['token'])) {
+    $googleClient->setAccessToken($_SESSION['token']);
+}
+if ($googleClient->getAccessToken()) {
+    $plus = new Google_Service_Plus($googleClient);
+    $userProfile = $plus->people->get('me');
+    echo '<pre>';
+    var_export($userProfile);
+    echo '</pre>';
+} else {
+    $authUrl = $googleClient->createAuthUrl();
+}
+if (isset($authUrl)) {
+    echo "<a href='$authUrl'>login</a>";
+}
