a854a4ec4a4ed31b72aa639f6c33b2b9a6c9f79b
Vladimir Kovpak <cn007b@gmail.com>
2015-12-04 17:55:27 +0200

    Ed. Added symfony another example to fork process.

--- ed/symfony/examples/bulletinBoard/src/AppBundle/Command/ProcessCommand.php
+++ ed/symfony/examples/bulletinBoard/src/AppBundle/Command/ProcessCommand.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Command;
 
 use duncan3dc\Helpers\Fork;
+use Spork\ProcessManager;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -10,7 +11,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * @example php app/console app:process --threadsCount=5
+ * @example php app/console app:process --engine=spork --threadsCount=5
  */
 class ProcessCommand extends ContainerAwareCommand
 {
@@ -18,6 +19,13 @@ class ProcessCommand extends ContainerAwareCommand
     {
         $this
             ->setName('app:process')
+            ->addOption(
+                'engine',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'engine',
+                'duncan3dc'
+            )
             ->addOption(
                 'threadsCount',
                 null,
@@ -30,19 +38,39 @@ class ProcessCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $engine = $input->getOption('engine');
         $threadsCount = $input->getOption('threadsCount');
+        $this->$engine($threadsCount);
+    }
+
+    final private function duncan3dc($threadsCount)
+    {
         $fork = new Fork();
         foreach (range(1, $threadsCount) as $i) {
             $fork->call(function () use ($i) {
-                $this->doSomeStuff($i);
+                $r = $this->doSomeStuff($i);
+                echo "duncan3dc: $r".PHP_EOL;
             });
         }
         $fork->wait();
     }
 
-    public function doSomeStuff($i)
+    final private function spork($threadsCount)
     {
-        echo "Process $i".PHP_EOL;
+        $manager = new ProcessManager();
+        $that = $this;
+        foreach (range(1, $threadsCount) as $i) {
+            $manager->fork(function () use ($i, $that) {
+                return $that->doSomeStuff($i);
+            })->then(function ($fork) {
+                echo "spork: Pid: {$fork->getPid()} says: {$fork->getResult()}".PHP_EOL;
+            });
+        }
+    }
+
+    final private function doSomeStuff($i)
+    {
         sleep(2);
+        return "Processed $i";
     }
 }
