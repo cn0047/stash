d88bbd16b30cba9319af40abcc822a1e0bd20fe2
Vladimir Kovpak <cn007b@gmail.com>
2015-12-15 08:44:06 +0200

    Ed. Add symfony form example.

--- ed/symfony/examples/FancySurvey/src/AppBundle/Form/Type/Step1.php
+++ ed/symfony/examples/FancySurvey/src/AppBundle/Form/Type/Step1.php
@@ -20,6 +20,52 @@ class Step1 extends AbstractType
             ;
     }
 
+    public function buildFormUs(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date_from', 'text_not_null', [
+                'required'    => true,
+                'constraints' => [
+                    new Constraints\NotBlank(['message' => t('param_exception_missing', 'date_from')]),
+                    new DateTime(['message' => t('param_exception_invalid_datetime_value', 'date_from')]),
+                ]
+            ])
+            ->add('date_to', 'text_not_null', [
+                'required'    => true,
+                'constraints' => [
+                    new Constraints\NotBlank(['message' => t('param_exception_missing', 'date_to')]),
+                    new DateTime(['message' => t('param_exception_invalid_datetime_value', 'date_to')]),
+                ]
+            ])
+            ->add('limit', 'text_not_null', [
+                'empty_data'  => 50,
+                'required'    => false,
+                'constraints' => [
+                    new Constraints\Range(
+                        [
+                            'minMessage'     => t('param_exception_invalid_more_value', 'limit'),
+                            'maxMessage'     => t('param_exception_invalid_less_value', 'limit'),
+                            'invalidMessage' => t('param_exception_invalid_number_value', 'limit'),
+                            'min'            => 1,
+                            'max'            => 100,
+                        ]),
+                ]
+            ])
+            ->add('offset', 'text_not_null', [
+                'empty_data'  => 0,
+                'required'    => false,
+                'constraints' => [
+                    new Constraints\GreaterThanOrEqual(
+                        [
+                            'message' => t('param_exception_invalid_greater_or_equal_value', 'offset'),
+                            'value'   => 0,
+                        ]),
+                ]
+            ]);
+
+        parent::buildForm($builder, $options);
+    }
+
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
