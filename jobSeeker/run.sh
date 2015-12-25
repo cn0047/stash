1372967b46aed95ab3a25094549da54824201089
Vladimir Kovpak <cn007b@gmail.com>
2015-12-25 19:55:05 +0200

    Added jobSeeker.

---
+++ jobSeeker/run.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+readonly START_FROM_ID_OFFSET=22594
+
+args="$@"
+idFrom=$1
+
+if [ -z "$idFrom" ]; then
+    echo "Finding id"
+    i=0
+    while [ true ]; do
+        id=$(($START_FROM_ID_OFFSET+$i))
+        i=$(($i+1))
+        curl -is -o /tmp/jobSeeker.tmp "http://jobs.dou.ua/companies/designcontestcom/vacancies/$id/"
+        is404=$((`grep 'HTTP/1.1 404 NOT FOUND' -ric /tmp/jobSeeker.tmp`))
+        printf "\ri = $i, id = $id, is 404 = $is404"
+        if [ $is404 -eq 0 ];
+        then
+            printf "\nFound id - $id\n"
+            break
+        fi
+        rm /tmp/jobSeeker.tmp
+        is404=0
+    done
+else
+    echo "Start from $idFrom"
+    i=0
+    while [ true ]; do
+        id=$(($idFrom+$i))
+        i=$(($i+1))
+        url="http://jobs.dou.ua/companies/designcontestcom/vacancies/$id/"
+        curl -is -o /tmp/jobSeeker.tmp "$url"
+        is404=$((`grep 'HTTP/1.1 404 NOT FOUND' -ric /tmp/jobSeeker.tmp`))
+        isPhp=$((`grep 'php|пхп' -Eric /tmp/jobSeeker.tmp`))
+        isRemote=$((`grep 'remote|удален|відд?ален' -Eric /tmp/jobSeeker.tmp`))
+        printf "404 = $is404, php = $isPhp, remote = $isRemote, url = $url \n"
+        if [ $is404 -eq 1 ];
+        then
+            printf "Found 404.\n"
+            break
+        fi
+        rm /tmp/jobSeeker.tmp
+        is404=0
+    done
+fi
---
+++ jobSeeker/run2.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+readonly URL_1='http://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D1%97%D0%B2&category=PHP&count=9999'
+readonly URL_2='http://jobs.dou.ua/vacancies/xhr-load/?category=PHP&city=%D0%9A%D0%B8%D1%97%D0%B2&count_BAD=9999'
+
+curl -s -o /tmp/jobSeeker.out.1.tmp -c /tmp/jobSeeker.cookie.tmp $URL_1
+token=`grep 'csrftoken(.*)' -Erio /tmp/jobSeeker.cookie.tmp | awk '{print $2}'`
+curl -s -X POST -o /tmp/jobSeeker.out.xhr.2.tmp -c /tmp/jobSeeker.cookie.tmp -b /tmp/jobSeeker.cookie.tmp -d count=100 -d csrfmiddlewaretoken=$token $URL_2
+php -r 'var_export(json_decode(`cat /tmp/jobSeeker.out.xhr.2.tmp`, 1));' > /tmp/jobSeeker.out.2.tmp
+rm `ls /tmp/jobSeeker.out.xhr.*.tmp`
+subl `ls /tmp/jobSeeker.out*`
+
+grep 'https?:\/\/.*\/vacancies\/[0-9]+' -Erin /tmp/jobSeeker.out.* | wc -l
+grep 'https?:\/\/.*\/vacancies\/[0-9]+' -Erin /tmp/jobSeeker.out.*
+grep 'jobs.dou.ua\/companies' -Erio /tmp/jobSeeker.out.*|wc -l
+
+rm `ls /tmp/jobSeeker.*`
+ls /tmp/jobSeeker.*
+
+# $RANDOM
+$('.vacancy').size()
