d485bb52f7dc84b58c6681205f5b4453ff0b2fc2
Vladimir Kovpak <cn007b@gmail.com>
2015-12-27 12:24:40 +0200

    Updated jobSeeker.

--- jobSeeker/rabota-ua.sh
+++ jobSeeker/rabota-ua.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-readonly URL='http://www.work.ua/jobs-kyiv-php/?days=123&page='
+readonly DATE=`date "+%d.%m.%Y" -d "7 days ago"`
+readonly URL="http://rabota.ua/jobsearch/vacancy_list?regionId=1&keyWords=php&period=3&lastdate=$DATE&pg=" # page
 
 toOpen=''
 i=0
 while [ true ]; do
     i=$(($i+1))
     curl -s -o /tmp/jobSeeker.tmp "$URL$i"
-    is404=$((`grep 'card-logotype' -ric /tmp/jobSeeker.tmp`))
-    if [ $is404 -eq 0 ]; then
+    is404=$((`grep 'не нашли ничего' -Eric /tmp/jobSeeker.tmp`))
+    if [ $is404 -gt 0 ]; then
         echo $toOpen
-        # /usr/bin/chromium-browser $toOpen
+        /usr/bin/chromium-browser $toOpen
         exit
     fi
-    for uri in `grep '\/jobs\/[0-9]+\/' -Erio /tmp/jobSeeker.tmp`; do
-        curl -s -o /tmp/jobSeeker.item.tmp "http://www.work.ua$uri"
+    for uri in `grep '\/company[0-9]+\/vacancy[0-9]+' -Erio /tmp/jobSeeker.tmp`; do
+        curl -s -o /tmp/jobSeeker.item.tmp "http://rabota.ua$uri"
         isInteresting=$((`grep 'remote|удален|відд?ален' -Eric /tmp/jobSeeker.item.tmp`))
         exclude=$((`grep '1С|удаленную работу не рассматрива|Вариант работы удаленно не рассматриваем' -Eric /tmp/jobSeeker.item.tmp`))
         addedToHeap=0
@@ -22,7 +23,7 @@ while [ true ]; do
             if [ $exclude -eq 0 ]; then
                 addedToHeap=1
                 if [ "$toOpen" != *"$uri"* ]; then
-                    toOpen="$toOpen http://www.work.ua$uri"
+                    toOpen="$toOpen http://rabota.ua$uri"
                 fi
             fi
         fi
