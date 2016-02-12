e9a98c195490508d026f6e0b5b20c029cc234af3
Vladimir Kovpak <cn007b@gmail.com>
2016-02-12 23:52:09 +0200

    Ed. Js bubbleSort.

--- ed/js/examples/bubbleSort.js
+++ ed/js/examples/bubbleSort.js
@@ -1,11 +1,12 @@
-var s = ['f','s','w','e','l','x','v','d','a'];
-var n = s.length;
+var a = ['f','s','w','e','l','x','v','d','a'];
+var n = a.length;
 for (i = 0; i < n; i++) {
-    for (j = n - i - 1; j > 0; j--) {
-        if (s[j] < s[j-1]) {
-            var v = s[j];
-            s[j] = s[j-1];
-            s[j-1] = v;
+    for (j = 0; j < n - i - 1 ; j++) {
+        if (a[j] > a[j + 1]) {
+            var v = a[j + 1];
+            a[j + 1] = a[j];
+            a[j] = v;
         }
     }
 }
+console.log(a);
