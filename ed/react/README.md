b72b3b04b661601dff42e7d31d6a6407bf1082b4
Vladimir Kovpak <cn007b@gmail.com>
2016-12-27 13:12:36 +0200

    Ed. React.

---
+++ ed/react/README.md
@@ -0,0 +1,9 @@
+React
+-
+
+````
+npm install -g create-react-app
+create-react-app hello-world
+cd hello-world
+npm start
+````
--- vagrant/common/provision.before.sh
+++ vagrant/common/provision.before.sh
@@ -16,6 +16,11 @@ sudo apt-get install -y nginx
 # git
 sudo apt-get install -y git
 
+# nodejs
+sudo apt-get install python-software-properties
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install nodejs
+
 # mysql
 # # sudo apt-get install -y mysql-server
 # # mysql -uroot -e 'create database test'
