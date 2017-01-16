d9e6856bdfc656661edc0c3679d74494f8d3f2a1
Vladimir Kovpak <cn007b@gmail.com>
2017-01-16 09:03:30 +0200

    Ed. Php.

--- ed/php/examples/yield.php
+++ ed/php/examples/yield.php
@@ -1,21 +1,21 @@
 <?php
 
-$a = [1, 2, 3, 4, 5];
+$a = range(1, 1000);
 
-$t1 =microtime();
+$t1 = microtime();
 $m1 = memory_get_usage();
-function genOneToThree ($a)
+function getNext($a)
 {
     foreach ($a as $v) {
         yield $v;
     }
 }
-foreach (genOneToThree($a) as $v) {
+foreach (getNext($a) as $v) {
     echo $v.PHP_EOL;
 }
 $t1 = microtime() - $t1;
 $m1 = memory_get_usage() - $m1;
-$t2 =microtime();
+$t2 = microtime();
 $m2 = memory_get_usage();
 foreach ($a as $v) {
     echo $v.PHP_EOL;
