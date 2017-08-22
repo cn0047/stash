bfe95e1343fbd1e4b4e5c98b492f2cfa8cbaa7f0
Vladimir Kovpak <cn007b@gmail.com>
2017-08-22 07:07:19 +0300

    Ed. Algorithms.

---
+++ ed/algorithms/basic/lcdAndLcm.js
@@ -0,0 +1,17 @@
+/**
+ * Largest (Greatest) Common Divider.
+ */
+function lcd(a, b) {
+   if (b == 0) return a;
+   return gcd(b, a % b);
+}
+
+console.log(gcd(4, 6)); // 2
+console.log(gcd(15, 25)); // 5
+
+/**
+ * Least Common Multiple.
+ */
+function lcm(a, b) {
+  return a / lcd(a, b) * b;
+}
