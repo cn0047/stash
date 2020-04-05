d21948ad986c7bce4f3ad0b4fa4846bfdf11dbfe
Vladimir Kovpak <cn007b@gmail.com>
2020-04-05 16:01:06 +0300

    Ed. Small updates.

--- ed/l/go/examples/whatever/hash.go
+++ ed/l/go/examples/whatever/hash.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/md5"
 	"crypto/sha1"
+	"crypto/sha256"
 	"fmt"
 )
 
@@ -14,4 +15,8 @@ func main() {
 	sh1 := sha1.New()
 	sh1.Write([]byte(s))
 	fmt.Printf("sha1 = %x \n", sh1.Sum(nil))
+
+	sh256 := sha256.New()
+	sh256.Write([]byte(s))
+	fmt.Printf("sha256 = %x \n", sh256.Sum(nil))
 }
