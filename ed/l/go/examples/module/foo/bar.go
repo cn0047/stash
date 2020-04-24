8f699e63386881a258ee280640125834288c44c9
Vladimir Kovpak <cn007b@gmail.com>
2020-04-24 19:20:56 +0300

    Added go thrift example.

---
+++ ed/l/go/examples/module/foo/bar.go
@@ -0,0 +1,9 @@
+package foo
+
+import (
+	"github.com/thepkg/strings"
+)
+
+func GetStr(s string) string {
+	return strings.ToUpperFirst(s)
+}
---
+++ ed/l/go/examples/module/go.mod
@@ -0,0 +1,5 @@
+module mdl
+
+go 1.13
+
+require github.com/thepkg/strings v1.1.2
