da9fcade02f751879aaad8d53c2eb4b96a05b3f5
Vladimir Kovpak <cn007b@gmail.com>
2020-06-09 10:08:52 +0300

    Updates in ml examples.

--- ed/ai/ml/tensorflow/examples.sh
+++ ed/ai/ml/tensorflow/examples.sh
@@ -9,4 +9,8 @@ source ./venv/bin/activate
 pip3 install numpy matplotlib tensorflow tensorboard
 python3 regression.py
 tensorboard --logdir=/tmp/tf_logs --bind_all
+open http://localhost:6006/
 deactivate
+# or
+docker run -ti --rm --net=xnet -p 6006:6006 -v $PWD:/gh -w /gh cn007b/python /bin/bash
+# ⬆
