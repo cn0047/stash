e412c9a7011ff8446d12d71b54cfed4fcf2fc54c
Vladimir Kovpak <cn007b@gmail.com>
2020-06-17 06:41:57 +0300

    Small updates.

--- ed/sh/docker/examples.neo4j.sh
+++ ed/sh/docker/examples.neo4j.sh
@@ -5,6 +5,10 @@ docker run -it --rm --net=xnet -p 7474:7474 -p 7687:7687 --name xneo4j --hostnam
   -v $PWD/.data/.docker/neo4j:/data \
   neo4j:3.5
 
+  # -e NEO4J_dbms_connector_https_enabled=false \
+  # -e NEO4J_dbms_ssl_policy_https_enabled=false \
+  # -e NEO4J_dbms_ssl_policy_bolt_enabled=false \
+
 # usr=neo4j
 # pwd=test # prev_pwd=neo4j
 open http://0.0.0.0:7474/
