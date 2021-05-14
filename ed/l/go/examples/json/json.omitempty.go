76dd6b80133ca9b6d7a4d54522c8368ca4fa22b4
V. Kovpak <cn007b@gmail.com>
2021-05-14 12:48:12 +0300

    Ed. Added small go example.

--- ed/l/go/examples/json/json.omitempty.go
+++ ed/l/go/examples/json/json.omitempty.go
@@ -5,6 +5,14 @@ import (
 	"fmt"
 )
 
+func main() {
+	//f1()
+	//f2()
+	//f3()
+	// sliceOfStructs()
+	sliceOfStructsPointers()
+}
+
 type Foo struct {
 	Msg string `json:"msg,omitempty"`
 	Bar Bar    `json:"bar,omitempty"`
@@ -17,6 +25,14 @@ type Foo2 struct {
 
 type Bar struct {
 	Value string `json:"value,omitempty"`
+}
+
+type Bars struct {
+	Data []Bar `json:"data,omitempty"`
+}
+
+type BarsPointers struct {
+	Data []*Bar `json:"data,omitempty"`
 }
 
 func f1() {
@@ -46,8 +62,30 @@ func f3() {
 	fmt.Printf("[f3] Result: %s \n", j) // [f3] Result: {"bar":{}}
 }
 
-func main() {
-	//f1()
-	//f2()
-	f3()
+func sliceOfStructs() {
+	b := Bars{}
+	b.Data = []Bar{
+		{Value: "1"},
+		{Value: "2"},
+		{Value: ""},
+	}
+	j, err := json.Marshal(b)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("[sliceOfStructs] Result: %s \n", j) // Result: {"data":[{"value":"1"},{"value":"2"},{}]}
+}
+
+func sliceOfStructsPointers() {
+	b := BarsPointers{}
+	b.Data = []*Bar{
+		{Value: "1"},
+		{Value: "2"},
+		{Value: ""},
+	}
+	j, err := json.Marshal(b)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("[sliceOfStructsPointers] Result: %s \n", j) // [sliceOfStructsPointers] Result: {"data":[{"value":"1"},{"value":"2"},{}]}
 }
