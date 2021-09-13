d581d6647a983fe23d53b3f77cec5b89dcdf4356
V. Kovpak <cn007b@gmail.com>
2021-09-13 21:55:31 +0300

    Ed. Go AWS SNS.

---
+++ ed/l/go/examples/3rdparty/aws/src/app/sns/b.go
@@ -0,0 +1,39 @@
+package sns
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sns"
+)
+
+func Run(cfg *aws.Config) {
+	svc := sns.New(session.New(), cfg)
+	arn := "arn:aws:sns:${reg}:${id}:${name}"
+	publish(svc, arn)
+}
+
+func publish(svc *sns.SNS, arn string) {
+	payload := map[string]string{"foo": "bar2"}
+	j, err := json.Marshal(payload)
+	if err != nil {
+		panic(fmt.Sprintf("json err: %v", err))
+	}
+
+	input := &sns.PublishInput{
+		Message: aws.String(string(j)),
+		MessageAttributes: map[string]*sns.MessageAttributeValue{
+			"event_name": {DataType: aws.String("String"), StringValue: aws.String("EVENT_TYPE_FOO")},
+		},
+		TopicArn: aws.String(arn),
+	}
+	result, err := svc.Publish(input)
+	if err != nil {
+		fmt.Println("Publish error:", err)
+		return
+	}
+
+	fmt.Println(result)
+}
