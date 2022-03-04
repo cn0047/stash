27b2be283b3c003653330e345567ccf0e1d8f252
V.K <cn007b@gmail.com>
2022-03-04 15:26:50 +0100

    ► Open to see more.

--- ed/ad/array/triangleWithMaxPerimeter.js
+++ ed/ad/array/triangleWithMaxPerimeter.js
@@ -1,19 +1,19 @@
 /**
  * Gets MAX possible triangle perimeter from array with numbers.
  *
- * @param {array} A Input array with numbers.
+ * @param {array} arr Input array with numbers.
  * @return {number} Max possible triangle perimeter.
  */
-function f1(A) {
-    if (A.length === 3) return -1;
+function f1(arr) {
+    if (arr.length === 3) return -1;
     var max = 0;
-    while (A.length > 0) {
-      var v1 = A.shift();
-      for (var i = 0; i < A.length - 1; i++) {
-        for (var j = 1; j < A.length; j++) {
+    while (arr.length > 0) {
+      var v1 = arr.shift();
+      for (var i = 0; i < arr.length - 1; i++) {
+        for (var j = 1; j < arr.length; j++) {
           if (i === j) continue;
-          var v2 = A[i];
-          var v3 = A[j];
+          var v2 = arr[i];
+          var v3 = arr[j];
           if (v1 + v2 > v3 && v2 + v3 > v1 && v1 + v3 > v2) {
             var newMax = v1 + v2 + v3;
             if (newMax > max) {
@@ -29,16 +29,16 @@ function f1(A) {
 /**
  * Check does array contain triangle.
  *
- * @param {array} A Input array with numbers.
+ * @param {array} arr Input array with numbers.
  * @return {number} 0 in case array doesn't contain triangle, 1 - array contain triangle.
  */
-function f3(A) {
-  if (A.length < 3) return 0;
-  A.sort(function(a, b) { return a - b; });
-  for (var i = 0; i < A.length - 2; i++) {
-    var v1 = A[i];
-    var v2 = A[i + 1];
-    var v3 = A[i + 2];
+function f3(arr) {
+  if (arr.length < 3) return 0;
+  arr.sort(function(a, b) { return a - b; });
+  for (var i = 0; i < arr.length - 2; i++) {
+    var v1 = arr[i];
+    var v2 = arr[i + 1];
+    var v3 = arr[i + 2];
     if (
       (v1 + v2 > v3 && v2 + v3 > v1 && v1 + v3 > v2)
       && ( (v1 > 0) && (v2 > 0) && (v3 > 0) )
