ac81941b2e66342064d9ba352580002bdc3f5f1c
V.K <cn007b@gmail.com>
2022-03-25 15:25:17 +0100

    ► Open to see more.

--- ed/ad/array/minAbsDiff.go
+++ ed/ad/array/minAbsDiff.go
@@ -10,30 +10,32 @@ func main() {
 	fmt.Println(minimumAbsoluteDifference([]int32{1, -3, 71, 68, 17}))
 }
 
+// minimumAbsoluteDifference returns minimum absolute difference in an array.
+// The absolute difference is the positive difference between two values a and b.
+func minimumAbsoluteDifference(arr []int32) (diff int32) {
+	sort.Slice(arr, func(i, j int) bool { return arr[i] < arr[j] })
+
+	diff = modulo(arr[0] - arr[1])
+
+	if len(arr) == 2 {
+		return diff
+	}
+
+	for i := 1; i < len(arr)-1; i++ {
+		j := i + 1
+		currentDiff := modulo(arr[i] - arr[j])
+		if currentDiff < diff {
+			diff = currentDiff
+		}
+	}
+
+	return diff
+}
+
 func modulo(v int32) int32 {
 	if v < 0 {
 		return v * -1
 	}
 
 	return v
-}
-
-func minimumAbsoluteDifference(arr []int32) (r int32) {
-	sort.Slice(arr, func(i, j int) bool { return arr[i] < arr[j] })
-
-	r = modulo(arr[0] - arr[1])
-
-	if len(arr) == 2 {
-		return r
-	}
-
-	for i := 1; i < len(arr)-1; i++ {
-		j := i + 1
-		d := modulo(arr[i] - arr[j])
-		if d < r {
-			r = d
-		}
-	}
-
-	return r
 }
