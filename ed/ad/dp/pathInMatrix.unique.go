ff3940049d37b3559f5872db26500fb802a9932a
b0xter <codenamek2010@gmail.com>
2022-11-03 23:12:36 +0100

    Updated pathInMatrix.unique.go, added uniquePathsWithObstacles.
    Updated pathInMatrix.unique.go, added uniquePathsWithObstacles.
    
    Co-authored-by: b0xter <codenamek2010@gmail.com>
    Co-authored-by: cn007b <cn007b@gmail.com>

--- ed/ad/dp/pathInMatrix.unique.go
+++ ed/ad/dp/pathInMatrix.unique.go
@@ -7,8 +7,10 @@ import (
 func main() {
 	var r int
 
-	r = uniquePaths(3, 2) // expected 3
-	r = uniquePaths(3, 7) // expected 28
+	// r = uniquePaths(3, 2) // expected 3
+	// r = uniquePaths(3, 7) // expected 28
+
+	r = uniquePathsWithObstacles([][]int{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}) // expected 2
 
 	fmt.Printf("Result: %v", r)
 }
@@ -43,4 +45,34 @@ func dump(arr [][]int) {
 	for _, vector := range arr {
 		fmt.Printf("%v \n", vector)
 	}
+}
+
+// uniquePathsWithObstacles returns count of unique paths without obstacles to get to bottom-right corner.
+// @see: https://leetcode.com/problems/unique-paths-ii/
+func uniquePathsWithObstacles(obstacles [][]int) int {
+	m := len(obstacles)
+	n := len(obstacles[0])
+	cache := make([][]int, m)
+	for i := 0; i < m; i++ {
+		cache[i] = make([]int, n)
+	}
+
+	cache[0][0] = 1
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if obstacles[i][j] == 1 {
+				cache[i][j] = 0
+			} else if i > 0 && j > 0 {
+				cache[i][j] = cache[i-1][j] + cache[i][j-1]
+			} else if i > 0 {
+				cache[i][j] = cache[i-1][j]
+			} else if j > 0 {
+				cache[i][j] = cache[i][j-1]
+			}
+		}
+	}
+	dump(cache)
+
+	return cache[m-1][n-1]
 }
